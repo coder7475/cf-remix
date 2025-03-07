8a6125343c56afd566cebbd1444b4f1036f6eaf9
coder7475 <robiulhossain7475@gmail.com>
2025-03-07 10:51:47 +0600

    fix: tailwind config

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,8 +2,8 @@ import type { MetaFunction } from "@remix-run/cloudflare";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Robiul Hossain" },
+    { name: "description", content: "Software Engineer & DevOps Engineer" },
   ];
 };
 
