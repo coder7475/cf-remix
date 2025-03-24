c6f821401115bf738704eca44c49bc671d1c2bf8
coder7475 <robiulhossain7475@gmail.com>
2025-03-24 19:47:54 +0600

    feat: fix import in blog

--- app/components/Blog.tsx
+++ app/components/Blog.tsx
@@ -1,9 +1,9 @@
 
 import { useState, useEffect, useRef } from "react";
-import { cn } from "@/lib/utils";
+import { cn } from "~/lib/utils";
 import { CalendarIcon, Clock, ArrowRight } from "lucide-react";
 import axios from "axios";
-import { useToast } from "@/hooks/use-toast";
+import { useToast } from "~/hooks/use-toast";
 
 interface BlogPost {
   id: number;
